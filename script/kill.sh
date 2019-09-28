e517c5dc2da99710e75b9b33292d4ea7129e28a3
moogates <moogates@163.com>
2019-09-28 21:29:20 +0800

    memcached test script

--- script/kill.sh
+++
@@ -1,2 +0,0 @@
-ps axu | grep mcproxy$ | awk '{print $2}'
-ps axu | grep mcproxy$ | awk '{print $2}' | xargs kill -9
---
+++ script/memcached/get5.sh
@@ -0,0 +1,5 @@
+query="get key1 key2 key3 key4 key5 key6 key7 key8\r\nget key1 key2 key3 key4 key5 key6 key7 key8\r\n"
+echo "--------- $query -----------"
+printf "$query" | nc 127.0.0.1 11311 | grep "VALUE\|END"
+echo
+
